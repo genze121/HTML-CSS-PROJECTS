0243e9d22bc986799012bc0af3c10b35af0dfde9
Tirtha Sharma <tirthagenze121@gmail.com>
2025-01-04 11:53:44 +0530

    Scroll Back To Top.

---
+++ Buttons/Scroll To Top/script.js
@@ -0,0 +1,18 @@
+const scrollButton = document.querySelector("#top-button");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
