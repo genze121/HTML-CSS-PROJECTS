0847fdcd51fa66057c3c14d6729e544a3c84f902
Tirtha Sharma <tirthagenze121@gmail.com>
2025-01-13 11:17:49 +0530

    Callout.

---
+++ Callout/script.js
@@ -0,0 +1,7 @@
+const closeIcon = document.querySelector(".fa");
+const calloutDiv = document.querySelector(".callout-two");
+
+closeIcon.addEventListener("click", () => {
+  calloutDiv.style.display = "none";
+});
+ 
