fe7a4ade779c3bfecc7e4b3dd8ff65d2343909d3
Tirtha Sharma <tirthagenze121@gmail.com>
2025-01-27 19:27:23 +0530

    Contact chips.

---
+++ Contact-Chips/script.js
@@ -0,0 +1,6 @@
+const contactChip = document.querySelector(".close-chip");
+const crossIcon = document.querySelector(".cross-icon");
+
+crossIcon.addEventListener("click", () => {
+  contactChip.style.display = "none";
+});
