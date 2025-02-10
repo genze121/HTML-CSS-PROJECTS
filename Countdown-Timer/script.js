6434a0bfd7b27564074c83cdc7e97555c97cf9e5
Tirtha Sharma <tirthagenze121@gmail.com>
2025-02-10 23:34:01 +0530

    Countdown TImer.

---
+++ Countdown-Timer/script.js
@@ -0,0 +1,19 @@
+const countTimer = document.querySelector(".count-timer");
+
+const countTimerElemet = new Date("Feb 10, 2025 15:37:25").getTime();
+
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countTimerElemet - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor(distance % (1000 * 60) / 1000);
+  countTimer.innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    countTimer.innerHTML = "Expired";
+  }
+}, 1000);
