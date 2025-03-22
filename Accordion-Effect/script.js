13709db21975124f0126152c92b2211f6c68d298
Tirtha Sharma <tirthagenze121@gmail.com>
2025-03-22 11:51:13 +0530

    Accordion.

---
+++ Accordion-Effect/script.js
@@ -0,0 +1,13 @@
+const accordionData = document.querySelectorAll(".accordion");
+
+accordionData.forEach(element => {
+  element.addEventListener("click", () => {
+    for (let i = 0; i < accordionData.length; i++) {
+      if (accordionData[i] !== element) {
+        accordionData[i].classList.remove("active");
+      } else {
+        element.classList.toggle("active");
+      }
+    }
+  });
+});
