a98421cd9257ce688ba052e2a3035519911cae47
Tirtha Sharma <tirthagenze121@gmail.com>
2025-04-07 23:39:13 +0530

    Automatic slider.

---
+++ Practice-Sets/Automatic-Slider-Gallery/script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let j = 0; j < dots.length; j++) {
+    dots[j].classList.remove("active");
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 1500);
+}
