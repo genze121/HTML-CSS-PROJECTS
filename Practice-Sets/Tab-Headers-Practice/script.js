87e16d92deaca3b59379b7aca32af4269e0a15cc
Tirtha Sharma <tirthagenze121@gmail.com>
2025-04-10 11:08:06 +0530

    Tab Headers Practice set.

---
+++ Practice-Sets/Tab-Headers-Practice/script.js
@@ -0,0 +1,33 @@
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabLinks.forEach((element) => {
+  element.addEventListener("click", function () {
+    const tabName = this.dataset.tab;
+    const tabContent = document.getElementById(tabName);
+
+    for (let i = 0; i < tabContents.length; i++) {
+      tabContents[i].style.display = "none";
+    }
+
+    for (let j = 0; j < tabLinks.length; j++) {
+      if (tabLinks[j] === this) {
+        if (tabLinks[j].classList.contains("red")) {
+          tabLinks[j].style.backgroundColor = "red";
+        } else if (tabLinks[j].classList.contains("blue")) {
+          tabLinks[j].style.backgroundColor = "blue";
+        } else if (tabLinks[j].classList.contains("green")) {
+          tabLinks[j].style.backgroundColor = "green";
+        } else {
+          tabLinks[j].style.backgroundColor = "orangeRed";
+        }
+      } else {
+        tabLinks[j].style.backgroundColor = "";
+      }
+    }
+
+    tabContent.style.display = "block";
+  });
+});
+
+document.querySelector(".tab-links").click();
